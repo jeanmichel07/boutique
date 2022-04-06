e5395e284aa1bf45ab96577252f6c51bc2042063
jeanmichel07 <tombokely@gmail.com>
2022-04-06 17:21:52 +0300

    inscription

---
+++ migrations/Version20220406101104.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220406101104 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(100) DEFAULT NULL, ADD nom VARCHAR(100) NOT NULL, ADD prenom VARCHAR(100) DEFAULT NULL, ADD adresse VARCHAR(255) NOT NULL, ADD is_verified TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP pseudo, DROP nom, DROP prenom, DROP adresse, DROP is_verified');
+    }
+}
