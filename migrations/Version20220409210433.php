8787916095154494c7e4e60aac1f04831132c7dc
jeanmiche07 <tombokely@gmail.com>
2022-04-12 12:22:48 +0300

    mm

--- migrations/Version20220409210433.php
+++ migrations/Version20220409210433.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220406101104 extends AbstractMigration
+final class Version20220409210433 extends AbstractMigration
 {
     public function getDescription(): string
     {
